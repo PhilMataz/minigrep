5425fba96f9df7ee9f38ea751066e80cc8fc3ce9
Phillip Matambanadzo <hello@philmataz.dev>
2024-11-26 12:35:55 +0000

    refactor(cli): improve error handling and arg parsing
    Refactor command-line argument handling to use an iterator pattern instead of
    collecting args into a Vec.

--- src/lib.rs
+++ src/lib.rs
@@ -25,21 +25,29 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
+    pub fn build(
+            mut args: impl Iterator<Item = String>,
+        ) -> Result<Config, &'static str> {
+            args.next();
 
-        Ok(Config { 
-            query, 
-            file_path, 
-            ignore_case 
-        })
-    }
+            let query = match args.next() {
+                Some(arg) => arg,
+                None => return Err("Didn't get a query string"),
+            };
+
+            let file_path = match args.next() {
+                Some(arg) => arg,
+                None => return Err("Didn't get a query file path"),
+            };
+            
+            let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+            Ok(Config { 
+                query, 
+                file_path, 
+                ignore_case 
+            })
+        }
 }
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
